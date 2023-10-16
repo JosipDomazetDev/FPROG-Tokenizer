dfbcf0da8802d67c245ca86137d1ec34fe5c02fe
Josip Domazet <josip.domazet.corp@gmail.com>
2023-10-16 09:45:22 +0200

    added tests for step 1-5

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ read_file = lambda filename: [line.strip() for line in open(filename, 'r')]
 tokenize = lambda text: re.findall(r'\b\w+\b', text.lower())
 
 # Step 4: Filter words
-filter_words = lambda words, filter_list: list(filter(lambda word: word in filter_list, words))
+filter_words = lambda words, filter_list: list(
+    filter(lambda current_filter_word: current_filter_word in words, filter_list))
 
 # Step 5: Count occurrences
 count_occurrences = lambda words: reduce(lambda acc, word: {**acc, **{word: acc.get(word, 0) + 1}}, words, {})
-
 
--- tests.py
+++ tests.py
@@ -1,10 +1,23 @@
 import unittest
+from main import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class TestCase(unittest.TestCase):
+    def test_tokenize(self):
+        text = "This is a test sentence."
+        tokens = tokenize(text)
+        self.assertEqual(tokens, ["this", "is", "a", "test", "sentence"])
 
+    def test_filter_words(self):
+        words = ["apple", "banana", "cherry", "banana", "date"]
+        filter_list = ["banana", "date"]
+        filtered_words = filter_words(words, filter_list)
+        self.assertEqual(filtered_words, ["banana", "date"])
+
+    def test_count_occurrences(self):
+        words = ["apple", "banana", "cherry", "banana", "date"]
+        occurrences = count_occurrences(words)
+        self.assertEqual(occurrences, {"apple": 1, "banana": 2, "cherry": 1, "date": 1})
 
 if __name__ == '__main__':
     unittest.main()
